30c232201f5b738d29eb0e0a7394e9b081ac1977
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-01-08 17:36:08 +0530

    update project

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ const submitBtn = document.querySelector(".btn-submit")
 for (let i=0; i<button.length; i++) {
     button[i].addEventListener("click", (e) =>{
         localStorage.setItem('score', e.target.value);
-        document.getElementById('rating').innerHTML = `${i+1}`
     }) 
 }
 
 submitBtn.addEventListener("click", () =>{
     window.location.href = "rating.html";
 })
-
-// var score = localStorage.getItem("score");
-// document.getElementById("rating").innerHTML = score;
