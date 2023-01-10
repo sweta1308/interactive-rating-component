1ca0b23286ffa400bd9a39544a187d6226aee68c
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-01-10 12:15:37 +0530

    update rating javascript

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const button = document.querySelectorAll(".input");
 const submitBtn = document.querySelector(".btn-submit");
-let finalRate = document.querySelector("#rating-final");
 
 for (let i=0; i<button.length; i++) {
     button[i].addEventListener("click", () => {
-        console.log(button[i].value);
-        finalRate = button[i].value;
+        localStorage.setItem('input', button[i].value)
 
     })
 }
 
-
-
 submitBtn.addEventListener("click", () =>{
     window.location.href = "rating.html";
 })
-console.log(button[0].value);
---
+++ rating.js
@@ -0,0 +1,4 @@
+var score = localStorage.getItem("input");
+var finalRate = document.querySelector("#rating-final");
+
+finalRate.innerHTML = score;
